14d4173eef98fe84c0a574415b6c4422848160eb
odanoburu <bcclaro@gmail.com>
2019-07-03 17:49:34 -0300

    * some validation functions
    - best structure is still somewhat unclear to me

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,21 @@
 module Lib
-    ( someFunc
+    ( parseLexicographerFile
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Parse
+import Process
+import Data.Text (Text)
+--import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
+import Data.Functor(($>))
+--import System.FilePath
+
+
+parseLexicographerFile :: FilePath -> IO [Synset]
+parseLexicographerFile fileName =
+  do
+    content <- TIO.readFile fileName
+    case parseLexicographer fileName content of
+      Left err -> putStr err $> []
+      Right lexFileSynsetStatements ->
+        return $ processLexicographerFile lexFileSynsetStatements
--- src/Process.hs
+++ src/Process.hs
@@ -5,6 +5,7 @@ module Process where
 
 --import Control.Monad
 --import Data.Char
+import Data.Maybe
 import Data.List
 import Data.Text (Text)
 import qualified Data.Text as T
@@ -13,7 +14,7 @@ import Data.GenericTrie
 import Parse hiding (synset,synsets,lexicalIdentifier)
 
 data Synset = Synset
-  { lexicographerFileId  :: Int
+  { lexicographerFileId  :: Text
   , wordSenses           :: [WNWord]
   , relations            :: [SynsetRelation]
   , definition           :: Text
@@ -22,25 +23,25 @@ data Synset = Synset
   , sourcePosition       :: Int
   } deriving (Show,Eq)
 
+
 processLexicographerFile :: (Text, Int, [(Int, [SynsetStatement])]) -> [Synset]
--- RawSynsetStatement will become SynsetStatements after error reporting
-processLexicographerFile (_, lexicographerFileId, synsetsComponents) =
+processLexicographerFile (lexicographerFileName, _, synsetsComponents) =
   map go synsetsComponents
   where
     go (offset, synsetStatements) = toSynset (synsetSkeleton offset) synsetStatements
-    synsetSkeleton = Synset lexicographerFileId [] [] "" [] []
+    synsetSkeleton = Synset lexicographerFileName [] [] "" [] []
     toSynset synset [] = synset
     toSynset synset@Synset{wordSenses} (WordSenseStatement wnWord : remainingStatements) =
       toSynset (synset {wordSenses = wnWord : wordSenses}) remainingStatements
     toSynset synset@Synset{relations} (SynsetRelationStatement relation : remainingStatements) =
       toSynset (synset {relations = relation : relations}) remainingStatements
     toSynset synset (DefinitionStatement definition : remainingStatements) =
-      -- only one definition is allowed, and this has already been checked
+      -- only one definition is allowed, and this has already been checked (TODO)
       toSynset (synset {definition = definition}) remainingStatements
     toSynset synset@Synset{examples} (ExampleStatement example : remainingStatements) =
       toSynset (synset {examples = example : examples}) remainingStatements
     toSynset synset (FramesStatement frames : remainingStatements) =
-      -- only one frames statement is allowed, and this has already been checked
+      -- only one frames statement is allowed, and this has already been checked (TODO)
       toSynset (synset {frames = frames}) remainingStatements
 
 -- when to change LexicographerFile : Text to LexicographerFileId :
@@ -55,19 +56,67 @@ makeIndex synsets = fromList keyValuePairs
     synsetPairs synset@Synset{lexicographerFileId, wordSenses} =
       map (go synset lexicographerFileId) wordSenses
     go synset lexicographerFileId (WNWord (_, wordForm, lexicalIdentifier) _ _) =
-      (senseKey wordForm 1 -- [ ]
-       lexicographerFileId lexicalIdentifier, Right synset) -- [ ] will using left in all other wordSenses save space?
+      (senseKey wordForm lexicographerFileId lexicalIdentifier
+      , Right synset) -- [ ] will using left in all other wordSenses save space?
 
-senseKey :: Text -> Int -> Int -> Int -> String
+senseKey :: Text --Int -> Int
+  -> Text
+  -> Int -> String
 -- [ ] this is wrong
-senseKey lemma synsetType lexicographerFileId lexicalIdentifier =
-  intercalate ":" [T.unpack lemma ++ "%", show synsetType, show lexicographerFileId, show lexicalIdentifier]
+senseKey lemma lexicographerFileName lexicalIdentifier =
+  intercalate ":" [T.unpack lemma, T.unpack lexicographerFileName, show lexicalIdentifier]      
 
-checkSynsetRelation :: Index -> SynsetRelation -> Either String SynsetRelation
-checkSynsetRelation = _
+---- validation
+data Validation e a = Failure e | Success a deriving (Show,Eq)
 
-checkWordPointer :: Index -> WordPointer -> Either String WordPointer
-checkWordPointer = _
+instance Functor (Validation e) where
+  fmap _ (Failure e) = Failure e
+  fmap f (Success a) = Success (f a)
 
-checkWordSenses :: [WNWord] -> Either String [WNWord]
-checkWordSenses = _
+instance Semigroup e => Applicative (Validation e) where
+  --  pure :: a -> Validation e a
+  pure = Success
+  --(<*>) :: Validation e (a -> b) -> Validation e a -> Validation e b
+  Success f <*> Success a  = Success (f a)
+  Success _ <*> Failure e  = Failure e
+  Failure e <*> Success _  = Failure e
+  Failure e <*> Failure e' = Failure (e <> e')
+
+data WNError = MissingSynsetRelationTarget SynsetRelation
+  | MissingWordRelationTarget WordPointer
+  | UnsortedSynsetWordSenses [Text]
+  deriving (Eq,Show)
+  -- [] how to include source info?
+
+checkSynset :: Index -> Synset -> Validation [WNError] Synset
+checkSynset index synset =
+  Success synset <* undefined
+
+--- use <*> for validation, or <*? see
+--- https://www.reddit.com/r/haskell/comments/7hqodd/pure_functional_validation/
+
+checkSynsetRelation :: Index -> SynsetRelation -> Maybe WNError
+checkSynsetRelation index synsetRelation@(SynsetRelation _ (maybeLexFileName, wordForm, lexicalId)) =
+  if member wordSenseKey index
+  then Nothing
+  else Just $ MissingSynsetRelationTarget synsetRelation -- []
+  where
+    wordSenseKey = senseKey wordForm (fromJust maybeLexFileName) lexicalId
+
+checkWordPointer :: Index -> WordPointer -> Maybe WNError
+checkWordPointer index wordPointer@(WordRelation _ (maybeLexFileName, wordForm, lexicalId)) =
+  -- check pointer name too
+  if member wordSenseKey index
+  then Nothing
+  else Just $ MissingWordRelationTarget wordPointer -- []
+  where
+    wordSenseKey = senseKey wordForm (fromJust maybeLexFileName) lexicalId
+
+checkWordSensesOrder :: [WNWord] -> Maybe WNError
+checkWordSensesOrder wordSenses =
+  if sortedWordForms /= wordForms
+  then Just $ UnsortedSynsetWordSenses sortedWordForms
+  else Nothing
+  where
+    sortedWordForms = sort wordForms
+    wordForms = map (\(WNWord (_, wordForm,_) _ _) -> wordForm) wordSenses
