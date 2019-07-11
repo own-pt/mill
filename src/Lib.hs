7999adda21391ac429bef6c6e1d9a6686ead2be4
odanoburu <bcclaro@gmail.com>
2019-07-11 17:49:39 -0300

    * WIP: parse configuration files
    - only lexnames for now

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,16 +4,20 @@ module Lib
     , validateLexicographerFile
     ) where
 
-import Parse hiding (synsets)
+import Parse (Synset,Unvalidated,parseLexicographer)
 import Validate
 
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as M
+import Control.Monad (unless,(>>))
 import Data.Either
---import Data.GenericTrie
---import Data.Text (Text)
---import qualified Data.Text as T
+import Data.Text (Text)
+import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
+import qualified Data.Text.Read as TR
 import Data.Functor(($>),void)
---import System.FilePath
+import System.FilePath ((</>))
+import System.Directory (doesDirectoryExist)
 
 
 parseLexicographerFile :: FilePath -> IO (Either () [Synset Unvalidated])
@@ -43,3 +47,32 @@ validateLexicographerFile fileToValidate otherFiles = do
           index   = makeIndex synsets
       in validation (mapM_ (TIO.putStrLn . showSourceError)) (void . return) $ validateSynsets index synsetsToValidate
     _ -> return ()
+
+data Config = Config
+  { lexnames :: Map Text Int
+  } deriving (Show,Eq)
+
+readTSV :: Ord a => FilePath -> ([Text] -> Either String (a, b)) -> IO (Map a b)
+readTSV filepath readLine = do
+  text <- TIO.readFile filepath
+  case go text of
+    ([], pairs) -> return $ M.fromList pairs
+    (errors, _) -> mapM_ putStrLn errors >> return M.empty
+  where
+    go = partitionEithers . map readLine . filter (not . isComment) . map (T.splitOn "\t") . drop 1 . T.lines
+    isComment [field] = let field' = T.strip field in T.take 2 field' == "--"
+    isComment _ = False
+
+readConfig :: FilePath -> IO Config
+readConfig configurationDir = do
+  isDirectory <- doesDirectoryExist configurationDir
+  unless isDirectory (putStrLn "Filepath must be a directory")
+  lexnames' <- readTSV (configurationDir </> "lexnames.tsv") lexnamesReader
+  return $ Config lexnames'
+  where
+    lexnamesReader [lexnameIdStr, lexicographerFile, _] =
+      case TR.decimal lexnameIdStr of
+        Left err -> Left err
+        Right (lexnameId, "") -> Right (lexicographerFile, lexnameId)
+        Right (_, trailing) -> Left $ "Trailing garbage after " ++ T.unpack trailing
+    lexnamesReader _ = Left "Wrong number of fields"
