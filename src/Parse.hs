48181c79ea6fb9579604751e8ec7ab6cca2c987a
odanoburu <bcclaro@gmail.com>
2019-08-30 17:32:52 -0300

    * relations.tsv
    - use commas for pos column
    - add satellite adjectives PoS
    
    * validation
    
    - check that a relation has the correct domain (either synset or
      wordsense of the appropriate PoS)
    
    obs: maybe this is too much to check in the parser phase and we should
    defer it to validation? (the parser is not so easy to read now)
    
    related to #6

--- src/Parse.hs
+++ src/Parse.hs
@@ -22,19 +22,22 @@ import qualified Text.Megaparsec.Char.Lexer as L
 --import Text.Megaparsec.Debug (dbg)
 import qualified Data.Set as S
 
+fst3 :: (a,b,c) -> a
+fst3 (first,_,_) = first
 
 type RawSynset = Either (ParseError Text Void) (Synset Unvalidated)
 
 -- State stores in which lexicographer file we're in, this is useful
 -- to fill in implicit references
-type Parser = ParsecT Void Text (Reader (LexicographerFileId, Map Text Text))
+type Parser = ParsecT Void Text (Reader (LexicographerFileId, Map Text Text, Map Text (NonEmpty WNObj, NonEmpty WNPOS)))
 
 parseLexicographer :: Map Text Text
+  -> Map Text (NonEmpty WNObj, NonEmpty WNPOS)
   -> String -> Text
   -> SourceValidation (NonEmpty (Synset Unvalidated))
-parseLexicographer relationsMap fileName inputText =
+parseLexicographer textToCanonicNames canonicToDomain fileName inputText =
   case runReader (runParserT lexicographerFile fileName inputText)
-                 (lexFileId, relationsMap) of
+                 (lexFileId, textToCanonicNames, canonicToDomain) of
     Right rawSynsets
       -> case partitionEithers (NE.toList rawSynsets) of
            ([], synsetsToValidate) -> Success $ NE.fromList synsetsToValidate
@@ -89,7 +92,7 @@ synsets = synsetOrError `NC.sepEndBy1` many linebreak
 synset :: Parser (Synset Unvalidated)
 synset = do
   startOffset      <- (1 +) <$> getOffset
-  lexicographerId  <- reader fst
+  lexicographerId  <- reader fst3
   synsetWordSenses <- wordSenseStatement `NC.endBy1` linebreak
   synsetDefinition <- definitionStatement <* linebreak
   synsetExamples   <- exampleStatement `endBy` linebreak
@@ -129,21 +132,30 @@ synsetRelationStatement :: Parser SynsetRelation
 synsetRelationStatement = L.nonIndented spaceConsumer go
   where
     go = SynsetRelation
-      <$> relationNameP synsetRelationName
+      <$> relationNameP SynsetObj synsetRelationName
       <*> (SynsetIdentifier <$> identifier)
     synsetRelationName = T.stripEnd
       -- [ ] handle this better
       <$> (takeWhile1P Nothing (`notElem` [':', ' ', '\n']) <?> "Synset relation name")
       <* symbol ":"
 
-relationNameP :: Parser Text -> Parser Text
-relationNameP name = do
-  relationsMap <- reader snd
-  relationName <- name
+relationNameP :: WNObj -> Parser Text -> Parser Text
+relationNameP obj name = do
+  LexicographerFileId (wnPOS, _) <- reader fst3
+  relationsMap     <- reader $ \(_,second,_) -> second
+  relationsDomains <- reader $ \(_,_,third) -> third
+  relationName     <- name
+  let relationString = T.unpack relationName
   case M.lookup relationName relationsMap of
-    Just _ -> if relationName `elem` ["d", "e", "fs", "w"]
+    Just canonicName -> if relationName `elem` ["d", "e", "fs", "w"]
               then fail "Synset components must come in the following order: words, definition, examples, frames, and synset relations"
-              else return relationName
+              else case M.lookup canonicName relationsDomains of
+                     Just (domain,poses) -> case (obj `elem` domain, wnPOS `elem` poses) of
+                       (True,True)  -> return relationName
+                       _            -> fail $ relationString
+                                            ++ " is not a valid relation for a " ++ show obj
+                                            ++ " with PoS " ++ show wnPOS
+                     Nothing -> error $ "Can't find " ++ T.unpack relationName ++ "'s domain"
     Nothing -> failure (Just $ toErrorItem relationName)
                        (S.fromList . map toErrorItem $ M.keys relationsMap)
   where
@@ -163,7 +175,7 @@ wordSenseIdentifier = WordSenseIdentifier <$> identifier
 identifier :: Parser (LexicographerFileId, WordSenseForm, LexicalId)
 identifier =
   (,,)
-  <$>  (try lexicographerIdentifier <|> reader fst)
+  <$>  (try lexicographerIdentifier <|> reader fst3)
   <*>  fmap WordSenseForm word <*> lexicalIdentifier
   where
     lexicographerIdentifier = do
@@ -175,7 +187,7 @@ wordSensePointers :: Parser [WordPointer]
 wordSensePointers = many go
   where
     go =  WordPointer
-      <$> relationNameP (word <?> "Word pointer")
+      <$> relationNameP WordObj (word <?> "Word pointer")
       <*> wordSenseIdentifier
 
 word :: Parser Text
